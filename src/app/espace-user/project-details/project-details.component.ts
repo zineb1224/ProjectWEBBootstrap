33e84f40218a15d5789843dd17293863ff8fb149
ZinebElhouz <zineb.elhouz@usmba.ac.ma>
2023-12-30 01:55:24 +0100

    update

--- src/app/espace-user/project-details/project-details.component.ts
+++ src/app/espace-user/project-details/project-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-project-details',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  public back(){
+    this.router.navigateByUrl("/espaceUser")
   }
 
 }
